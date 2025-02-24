4438c0403bc0c1f021e4fbfd3c5a4d310d47ab7e
Samuel Muvhango <93419130+muvhangos@users.noreply.github.com>
2025-02-24 10:24:24 +0200

    Delete chapter 4 directory

--- chapter 4/animals.py
+++
@@ -1,4 +0,0 @@
-animal= ['dog', 'horse', 'cow']
-for animal in animal:
- print(f"{animal.title()}, would made a good animal!")
- print(f"I love animals")
--- chapter 4/counting _to twenty.py
+++
@@ -1,2 +0,0 @@
-for value in range(1, 1000000):
- print(value)
