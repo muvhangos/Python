a75e31e701f3ece6c6d16a386b92e0280265e364
Samuel Muvhango <93419130+muvhangos@users.noreply.github.com>
2025-07-18 22:38:30 +0200

    Delete Projects/Project 2 directory

--- Projects/Project 2/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import greeting_page
-
-urlpatterns = [
-    path('', greeting_page, name='greeting_page'),
-]
--- Projects/Project 2/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def greeting_page(request):
-    return render(request, 'greetings/index.html')
